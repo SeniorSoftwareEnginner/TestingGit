88e1d5b936ddd7db9efe37907c18d09a22c0146e
SeniorSoftwareEnginner <supriya.ligade@gmail.com>
2017-02-13 13:51:12 +0530

    Add files via upload

---
+++ TrackingService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Locations;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+
+namespace GPSTracker
+{
+    [Service]
+    public class TrackingService : Service, ILocationListener
+    {
+        LocationManager locManager;
+        Criteria locationCriteria;
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+        {
+            locManager = (LocationManager)GetSystemService(LocationService);
+            locationCriteria = new Criteria();
+
+            locationCriteria.Accuracy = Accuracy.Coarse;
+            locationCriteria.PowerRequirement = Power.Low;
+
+            string locationProvider = locManager.GetBestProvider(locationCriteria, true);
+
+            // Preferences.MinTime, for example, 60 (seconds)
+            // Preferences.MinDist, for example, 100 (meters)
+            locManager.RequestLocationUpdates(locationProvider, 60 * 1000, 100, this);
+
+            return StartCommandResult.Sticky;
+        }
+
+        public override void OnTaskRemoved(Intent rootIntent)
+        {
+            Toast.MakeText(this, "OnTaskRemoved:: ", ToastLength.Long).Show();
+            base.OnTaskRemoved(rootIntent);
+        }
+
+        //#region LocationListener
+        public void OnLocationChanged(Location location)
+        {
+            Toast.MakeText(this, "location:: " + location.ToString(), ToastLength.Long).Show();
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+
+        }
+
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
+        {
+
+        }
+        //#endregion
+    }
+}
